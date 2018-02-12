b6bf8536cc004ab0bb572d54a5c157496ac600a9
Burak Tayfun <buara_91@hotmail.com>
2018-02-12 20:01:17 +0300

    UI Test update.

--- SteemitAppUITests/SteemitAppUITests.swift
+++ SteemitAppUITests/SteemitAppUITests.swift
@@ -12,25 +12,29 @@ class SteemitAppUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        addUIInterruptionMonitor(withDescription: "Push Dialog") { (alert) -> Bool in
+            let button = alert.buttons["OK"]
+            if button.exists {
+                button.tap()
+                return true
+            }
+            return false
+        }
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        app.textFields["Please Enter Username"].typeText("th")
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.buttons["Search"].tap()
     }
     
 }
