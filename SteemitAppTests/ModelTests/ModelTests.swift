36b8c7a1798deda5d8791c85101ba80e621964f7
Burak Tayfun <buara_91@hotmail.com>
2018-02-15 01:14:38 +0300

    Code coverage
    Slather added.

---
+++ SteemitAppTests/ModelTests/ModelTests.swift
@@ -0,0 +1,22 @@
+//
+//  ModelTests.swift
+//  SteemitAppTests
+//
+//  Created by Burak Tayfun on 2/15/18.
+//  Copyright © 2018 Burak Tayfun. All rights reserved.
+//
+
+import XCTest
+import OHHTTPStubs
+@testable import SteemitApp
+
+class ModelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
